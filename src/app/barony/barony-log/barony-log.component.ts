128aae956be5d86a363039b2e7440dc4a0df4d60
Leo Molinaro <leo.molinaro@sanmarcoinformatica.it>
2021-02-22 00:53:14 +0100

    .

--- src/app/barony/barony-log/barony-log.component.ts
+++ src/app/barony/barony-log/barony-log.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy, Input, OnChanges } from "@angular/core";
 import { SimpleChanges } from "@bg-utils";
-import { BaronyBoardService } from "../barony-board/barony-board.service";
+import { BaronyGameService } from "../barony-game/barony-game.service";
+import { BaronyContext } from "../logic";
 import { BaronyLand, BaronyLandCoordinates, BaronyLog, BaronyPawnType, BaronyPlayer } from "../models";
 
 interface BaronyLogStringFragment {
@@ -37,7 +38,7 @@ type BaronyLogFragment = BaronyLogStringFragment | BaronyLogPlayerFragment | Bar
 export class BaronyLogComponent implements OnChanges {
 
   constructor (
-    private service: BaronyBoardService
+    private context: BaronyContext
   ) { }
 
   @Input () log!: BaronyLog;
@@ -69,7 +70,7 @@ export class BaronyLogComponent implements OnChanges {
   } // string
 
   private player (playerId: string): BaronyLogPlayerFragment {
-    const player = this.service.getPlayer (playerId);
+    const player = this.context.getPlayer (playerId);
     return {
       type: "player",
       label: player.name,
@@ -78,7 +79,7 @@ export class BaronyLogComponent implements OnChanges {
   } // player
 
   private land (landId: BaronyLandCoordinates): BaronyLogLandFragment {
-    const land = this.service.getLand (landId);
+    const land = this.context.getLand (landId);
     let label: string;
     switch (land.type) {
       case "fields": label = "fields"; break;
