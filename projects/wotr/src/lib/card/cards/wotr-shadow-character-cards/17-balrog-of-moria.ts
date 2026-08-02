3246d86715708c0bfe05e1ba1ced1f2935af46ce
Leo Molinaro <rhapsody.leo@gmail.com>
2026-08-02 10:56:42 +0200

    balrog of moria combat scenario

--- projects/wotr/src/lib/card/cards/wotr-shadow-character-cards/17-balrog-of-moria.ts
+++ projects/wotr/src/lib/card/cards/wotr-shadow-character-cards/17-balrog-of-moria.ts
@@ -1,3 +1,4 @@
+import { advanceArmy, attack, retreatIntoSiege } from "../../../battle/wotr-battle-actions";
 import { declareFellowship } from "../../../fellowship/wotr-fellowship-actions";
 import { WotrScenario, WotrScenarioGroup } from "../../../scenario/wotr-scenario";
 import { WotrStoriesBuilder } from "../../../scenario/wotr-story-builder";
@@ -7,7 +8,7 @@ export function balrogOfMoria(): WotrScenarioGroup {
   return {
     id: "balrog-of-moria",
     name: "Balrog of Moria",
-    scenarios: [balrogOfMoria01]
+    scenarios: [balrogOfMoria01, balrogOfMoria02]
   };
 }
 
@@ -29,6 +30,34 @@ const balrogOfMoria01: WotrScenario = {
       b.fpT().firstPhaseDraw(),
       b.s().firstPhaseDraw(),
       b.fp().fellowshipPhase(declareFellowship("dimrill-dale"))
+    ]
+  })
+};
+
+const balrogOfMoria02: WotrScenario = {
+  id: "balrog-of-moria-02",
+  name: "Balrog of Moria",
+  description: "When the Balrog card is in play and the Shadow is battling near Moria",
+  loadDefinition: () => ({
+    setup: setupBuilder =>
+      setupBuilder
+        .shuffledDecks()
+        .region("lorien", "elves", { nRegulars: 1 })
+        .region("dimrill-dale", "sauron", { nRegulars: 1 })
+        .nation("sauron", true, "atWar")
+        .build(),
+    stories: (b: WotrStoriesBuilder) => [
+      b.fpT().firstPhaseDraw(),
+      b.s().firstPhaseDraw("Balrog of Moria"),
+      b.fp().fellowshipPhase(),
+      b.s().huntAllocation(0),
+      b.fpT().rollActionDice(),
+      b.s().rollActionDice("character", "army", "army"),
+      b.s().characterDie(playCardOnTable("Balrog of Moria")),
+      b.s().armyDie(attack("dimrill-dale", "lorien")),
+      b.fp().battleStory(retreatIntoSiege("lorien")),
+      b.s().battleStory(advanceArmy()),
+      b.s().armyDie(attack("lorien", "lorien"))
     ]
   })
 };
